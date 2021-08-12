c7ef4cdee92ba9c7d387502d8d4534530428e971
David Watson <david.s.watson11@gmail.com>
2021-08-12 20:28:14 +0200

    new shtuff

--- sim_v1.R
+++ sim_v1.R
@@ -30,7 +30,7 @@ beta_z_to_y <- sim_z_wts(n = 1000, d_z = 100, k = 50)
 #' @param d_z Dimensionality of Z.
 #' @param rho Auto-correlation of the Toeplitz matrix for Z.
 #' @param snr Signal-to-noise ratio. Either a scalar value, in which case it 
-#'   applies to both x and y, or a length-two vector.
+#'   applies to both X and Y, or a length-two vector.
 #' @param xzr X-to-Z ratio, i.e. the ratio of X-signal to Z-signal when X's
 #'   are not conditionally independent.
 
@@ -77,31 +77,34 @@ sim_dat <- function(n, d_z, rho, snr, xzr) {
   return(out)
 }
 
-# Ricardo's version: w := sum(sign(abs(coef0) - abs(coef1)))
-w_fn <- function(b, n, d_z, rho, snr, xzr) {
-  df <- sim_dat(n, d_z, rho, snr, xzr)
-  f0 <- cv.glmnet(x = df$z, y = df$h0$y)
-  f1 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h0$x)),
-                  y = df$h0$y)
-  v0 <- abs(coef(f0, s = 'lambda.min')[2:(d_z + 1), ])
-  v1 <- abs(coef(f1, s = 'lambda.min')[2:(d_z + 1), ])
-  w0 <- sum(sign(v0 - v1))
-  f0 <- cv.glmnet(x = df$z, y = df$h1$y)
-  f1 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h1$x)),
-                  y = df$h1$y)
-  v0 <- abs(coef(f0, s = 'lambda.min')[2:(d_z + 1), ])
-  v1 <- abs(coef(f1, s = 'lambda.min')[2:(d_z + 1), ])
-  w1 <- sum(sign(v0 - v1))
-  out <- data.table('w0' = w0, 'w1' = w1)
-  return(out)
-}
-res <- foreach(i = seq_len(2000), .combine = rbind) %dopar% 
-  w_fn(i, n = 1000, d_z = 100, rho = 0, snr = 5, xzr = 1)
 
-# Alternatively: w := ||beta_z0|| - ||beta_z1||
+# Define w as a difference in norms on Z weights
 eval_fn <- function(b, n, d_z, rho, snr, xzr, l) {
   # Simulate data (with fixed weights)
   df <- sim_dat(n, d_z, rho, snr, xzr)
+  # Function for computing redundancy of Z
+  s_fn <- function(models, l) {
+    # Extract Z weights
+    betas <- lapply(seq_len(4), function(i) {
+      coef(models[[i]], s = 'lambda.min')[2:(d_z + 1), ]
+    })
+    # Compute relevant norm
+    v <- lapply(seq_len(4), function(i) {
+      if (l == 0) {
+        sum(betas[[i]] == 0)
+      } else if (l == 1) {
+        sum(abs(betas[[i]]))
+      } else if (l == 2) {
+        sum(betas[[i]]^2)
+      }
+    })
+    # Take difference of differences
+    w0 <- v[[1]] - v[[2]]
+    w1 <- v[[3]] - v[[4]]
+    out <- w0 - w1
+    # Export
+    return(out)
+  }
   ### H0: X \indep Y | Z ###
   f0 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h0$x[sample.int(n)])),
                   y = df$h0$y)
@@ -111,22 +114,9 @@ eval_fn <- function(b, n, d_z, rho, snr, xzr, l) {
                   y = df$h0$x)
   f3 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h0$y)),
                   y = df$h0$x)
-  if (l == 1) {
-    v0 <- sum(abs(coef(f0, s = 'lambda.min')[2:(d_z + 1), ]))
-    v1 <- sum(abs(coef(f1, s = 'lambda.min')[2:(d_z + 1), ]))
-    v2 <- sum(abs(coef(f2, s = 'lambda.min')[2:(d_z + 1), ]))
-    v3 <- sum(abs(coef(f3, s = 'lambda.min')[2:(d_z + 1), ]))
-  } else if (l == 0) {
-    v0 <- sum(coef(f0, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-    v1 <- sum(coef(f1, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-    v2 <- sum(coef(f2, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-    v3 <- sum(coef(f3, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-  }
-  w0 <- v0 - v1
-  w1 <- v2 - v3
-  s_null <- w0 - w1
-  ### H1: X \dep Y | Z ###
-  f0 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h1$x[sample.int(n)])), 
+  s_null <- s_fn(list(f0, f1, f2, f3), l)
+  ### H1: X -> Y
+  f0 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h1$x[sample.int(n)])),
                   y = df$h1$y)
   f1 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h1$x)),
                   y = df$h1$y)
@@ -134,26 +124,13 @@ eval_fn <- function(b, n, d_z, rho, snr, xzr, l) {
                   y = df$h1$x)
   f3 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h1$y)),
                   y = df$h1$x)
-  if (l == 1) {
-    v0 <- sum(abs(coef(f0, s = 'lambda.min')[2:(d_z + 1), ]))
-    v1 <- sum(abs(coef(f1, s = 'lambda.min')[2:(d_z + 1), ]))
-    v2 <- sum(abs(coef(f2, s = 'lambda.min')[2:(d_z + 1), ]))
-    v3 <- sum(abs(coef(f3, s = 'lambda.min')[2:(d_z + 1), ]))
-  } else if (l == 0) {
-    v0 <- sum(coef(f0, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-    v1 <- sum(coef(f1, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-    v2 <- sum(coef(f2, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-    v3 <- sum(coef(f3, s = 'lambda.min')[2:(d_z + 1), ] == 0)
-  }
-  w0 <- v0 - v1
-  w1 <- v2 - v3
-  s_alt <- w0 - w1
+  s_alt <- s_fn(list(f0, f1, f2, f3), l)
   # Export
   out <- data.table('s_null' = s_null, 's_alt' = s_alt)
   return(out)
 }
 res <- foreach(i = seq_len(2000), .combine = rbind) %dopar% 
-  eval_fn(i, n = 1000, d_z = 100, rho = 0.1, snr = 5, xzr = 1, l = 0)
+  eval_fn(i, n = 1000, d_z = 100, rho = 0.1, snr = 5, xzr = 1, l = 2)
 
 
 # Variance about zero is a function of rho! (At least holding all else constant)
@@ -165,19 +142,60 @@ res <- foreach(i = seq_len(2000), .combine = rbind) %dopar%
 
 
 
-# Benchmarking against (i) simple Gaussian scores and (ii) Lasso coef counts
+# Benchmarking against:
+# (i) Entner's test (partial correlation test), assume 1 IV
+# (ii) Score-based: literally MSE with additive errors
+# (iii) Score-based: BIC or similar
+
 # First up, partial correlation test
-f0 <- cv.glmnet(x = df$z, y = df$h0$x)
-f1 <- cv.glmnet(x = df$z, y = df$h0$y)
-x_hat <- predict(f0, newx = df$z, s = 'lambda.min')
-y_hat <- predict(f1, newx = df$z, s = 'lambda.min')
-eps_x <- df$h0$x - x_hat
-eps_y <- df$h0$y - y_hat
-cor.test(eps_x, eps_y)
+trn <- sample(n, size = 0.8 * n)
+tst <- seq_len(n)[-trn]
+rho_fn <- function(h) {
+  if (h == 0) {
+    f0 <- cv.glmnet(x = df$z[trn], y = df$h0$x[trn])
+    f1 <- cv.glmnet(x = df$z[trn], y = df$h0$y[trn])
+  } else if (h == 1) {
+    f0 <- cv.glmnet(x = df$z[trn], y = df$h1$x[trn])
+    f1 <- cv.glmnet(x = df$z[trn], y = df$h1$y[trn])
+  }
+  x_hat <- predict(f0, newx = df$z[tst], s = 'lambda.min')
+  y_hat <- predict(f1, newx = df$z[tst], s = 'lambda.min')
+  if (h == 0) {
+    eps_x <- df$h0$x[tst] - x_hat
+    eps_y <- df$h0$y[tst] - y_hat
+  } else if (h == 1) {
+    eps_x <- df$h1$x[tst] - x_hat
+    eps_y <- df$h1$y[tst] - y_hat
+  }
+  pcor <- cor.test(eps_x, eps_y)
+  # BIC
+  bic_fn <- function(x, y) {
+    f <- cv.glmnet(x = x, y = y)
+    k <- sum(coef(f, s = 'lambda.min') != 0)
+    y_hat <- predict(f, newx = x, s = 'lambda.min')
+    eps_y <- y - y_hat
+    rmse <- sqrt(mean(eps_y^2))
+    bic <- -2 * sum(dnorm(eps_y, sd = rmse, log = TRUE)) + k * log(n)
+    return(bic)
+  }
+  bic_h0_f0_x <- bic_fn(x = df$z, y = df$h0$x)
+  bic_h0_f1_x <- bic_fn(x = cbind(df$z, df$h0$y), y = df$h0$x)
+  bic_h0_f0_y <- bic_fn(x = df$z, y = df$h0$y)
+  bic_h0_f1_y <- bic_fn(x = cbind(df$z, df$h0$x), y = df$h0$y)
+  bic_h1_f0_x <- bic_fn(x = df$z, y = df$h1$x)
+  bic_h1_f1_x <- bic_fn(x = cbind(df$z, df$h1$y), y = df$h1$x)
+  bic_h1_f0_y <- bic_fn(x = df$z, y = df$h1$y)
+  bic_h1_f1_y <- bic_fn(x = cbind(df$z, df$h1$x), y = df$h1$y)
+}
+
+
+# MSE
+
 
 # If Y <- Z -> X, then p(x,y|z) factorizes as p(x|z)p(y|z)
 # If Z -> X -> Y <- Z, then p(x,y|z) factorizes as p(x|z)p(y|x,z)
 # If Z -> Y -> X <- Z, then p(x,y|z) factorizes as p(y|z)p(x|y,z)
+# Calculate BIC for each and pick winner
 
 
 k <- sum(coef(f0, s = 'lambda.min') != 0)
@@ -190,7 +208,10 @@ bic <- -2 * sum(dnorm(eps_y, sd = rmse, log = TRUE)) + k * log(n)
 bic <- n * log(mean(eps^2)) + k * log(n)
 
 
-
+# Initialization: lasso E[X|Z] and E[Y|Z].
+# For every edge Z -> X: either add or delete (depending on which is a change)
+# and score results. Keep on adding/deleting until it doesn't help anymore.
+# 
 
 
 
@@ -264,10 +285,33 @@ res <- foreach(i = seq_len(2000), .combine = rbind) %dopar%
 
 
 
+# One idea: heteroskedastic errors as a failure mode for other methods
+# 
 
 
 
 
+
+# Ricardo's version: w := sum(sign(abs(coef0) - abs(coef1)))
+w_fn <- function(b, n, d_z, rho, snr, xzr) {
+  df <- sim_dat(n, d_z, rho, snr, xzr)
+  f0 <- cv.glmnet(x = df$z, y = df$h0$y)
+  f1 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h0$x)),
+                  y = df$h0$y)
+  v0 <- abs(coef(f0, s = 'lambda.min')[2:(d_z + 1), ])
+  v1 <- abs(coef(f1, s = 'lambda.min')[2:(d_z + 1), ])
+  w0 <- sum(sign(v0 - v1))
+  f0 <- cv.glmnet(x = df$z, y = df$h1$y)
+  f1 <- cv.glmnet(x = as.matrix(cbind(df$z, df$h1$x)),
+                  y = df$h1$y)
+  v0 <- abs(coef(f0, s = 'lambda.min')[2:(d_z + 1), ])
+  v1 <- abs(coef(f1, s = 'lambda.min')[2:(d_z + 1), ])
+  w1 <- sum(sign(v0 - v1))
+  out <- data.table('w0' = w0, 'w1' = w1)
+  return(out)
+}
+res <- foreach(i = seq_len(2000), .combine = rbind) %dopar% 
+  w_fn(i, n = 1000, d_z = 100, rho = 0, snr = 5, xzr = 1)
 
 
 
